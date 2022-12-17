5c39faa01201dac028397f61f6a1b9421735ca86
SvetlanaMedvedeva73 <sweta.medvedev16@yandex.ru>
2022-12-17 14:38:38 +0300

    dz1

---
+++ dz1/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+ */
+
+int n;
+Console.Write($"Введите N: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+PrintNums(1, n);
+
+void PrintNums(int left, int right)
+{
+    if (left == right || left >= right)
+        Console.Write($"{left} ");
+    else
+    {
+        Console.Write($"{left} ");
+        PrintNums(left + 1, right);
+    }
+}
+
