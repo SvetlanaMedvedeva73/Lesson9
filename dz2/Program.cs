59fdcf1e600b277fe73db905d15b6f0c173467cb
SvetlanaMedvedeva73 <sweta.medvedev16@yandex.ru>
2022-12-18 10:18:21 +0300

    dz2

---
+++ dz2/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+int m = 4; int n = 8;
+
+Console.Write($"{Summ(m, n)}");
+
+int Summ(int left, int right)
+{
+    if (left >= right)
+        return left;
+    return left + Summ(left + 1, right);
+}
